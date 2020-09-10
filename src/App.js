cffca8b662717847f770d68f45686623fc94b569
koganiurii <koganiurii@gmail.com>
2020-09-10 16:17:23 +0300

    front end templates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Route, Router } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import CreateExercise from "./components/CreateExercise";
+import ExerciseList from "./components/ExerciseList";
+import EditExercise from "./components/EditExercise";
+import CreateUser from "./components/CreateUser";
 
 function App() {
   return (
-    <div className="container">
-      <header>The App Works</header>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
