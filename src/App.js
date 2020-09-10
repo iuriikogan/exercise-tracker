7f400d0b3b647b44cfa266f350e4f9b25c3fe0b4
koganiurii <koganiurii@gmail.com>
2020-09-10 15:50:00 +0300

    exercise and user endpoints configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css"; 3
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header>The App Works</header>
     </div>
   );
