66a301fe3ae0d32e69c34705c98d27b23164fefd
koganiurii <koganiurii@gmail.com>
2020-09-16 11:46:48 +0300

    users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,35 +13,37 @@ router.route("/").post((req, res) => {
 
   // //check for existing user
 
-  User.findOne({ email }).then(User => {
-    if (User) return res.status(400).json({ msg: "user exists already" });
+  User.findOne({ email })
+    .then(user => {
+      if (user) return res.status(400).json({ msg: "user exists already" });
 
-    const newUser = new User({
-      username,
-      email,
-      password
-    });
-
-    //create salt and hash
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-          .save()
-          .then(user => {
-            res.json({
-              user: {
-                id: user.id,
-                name: user.name,
-                email: user.email
-              }
-            });
-          })
-          .catch(err => res.status(500).json("error: " + err));
+      const newUser = new User({
+        username,
+        email,
+        password
       });
-    });
-  });
+
+      //create salt and hash
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => {
+              res.json({
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email
+                }
+              });
+            })
+            .catch(err => res.status(500).json("error: " + err));
+        });
+      });
+    })
+    .catch(err => res.status(500).json("error: " + err));
 });
 
 //get all users
